bf1d0bda1efa0bf8964ef1aefb315a1403aceb3c
Tyreal Cosrin <battlemitten@gmail.com>
2021-07-15 02:32:00 -0400

    Part 2 of Assignment 7

--- DataStructures/Assign_7/main.cpp
+++ DataStructures/Assign_7/main.cpp
@@ -7,21 +7,42 @@ using namespace std;
 void numbers(ostream& outs, const string& prefix, unsigned int levels);
 
 void pattern(ostream& outs, unsigned int n, unsigned int i);
+
+
+int main(int argc, char **argv)
+{
+	numbers(cout, "THERBLIG", 2);
+    
+    cout << endl << endl << endl;
+    
+    pattern(cout, 8, 0);
+}
+
+// A loop to print exactly i spaces:
+ //for (k = 0; k < i; k++) outs << ' ';
+ // A loop to print n asterisks, each one followed by a space:
+// for (k = 0; k < n; k++) outs << "* ";
+
  // Precondition: n is a power of 2 greater than zero.
  // Postcondition: A pattern based on the above example has been
  // printed to the ostream outs. The longest line of the pattern has
  // n stars beginning in column i of the output. For example,
 // The above pattern is produced by the call pattern(cout, 8, 0).
-
-int main(int argc, char **argv)
-{
-	numbers(cout, "THERBLIG", 2);
-}
-
-
 void pattern(ostream& outs, unsigned int n, unsigned int i)
 {
-    
+    if (n == 0) 
+        return; //base case stops everything ofc
+
+    pattern(outs, n/2, i); // Each upper half, has to call first
+
+    for (int k = 0; k < i ; k++) outs << " "; // Central spacing
+
+    for (int k = 0; k < n; k++) outs << "* "; // Central string
+
+    outs << endl; //gaps to space everything out
+
+    pattern(outs, n/2, i+n/2); // Each below, has to print after obviously
+
 }
 
 void numbers(ostream& outs, const string& prefix, unsigned int levels)

e9d84d203d8ee52b94e88212a771a6e8c12aa918
Tyreal Cosrin <battlemitten@gmail.com>
2021-08-24 15:34:14 -0400

    File adjustments

--- DataStructures/DataStructures/main.cpp
+++ DataStructures/DataStructures/main.cpp
@@ -1,6 +1,89 @@
-#include <stdio.h>
 #include <iostream>
-
 using namespace std;
-
+ 
+/* A binary tree node has data, pointer to left child
+and a pointer to right child */
+struct Node {
+    int data;
+    struct Node *left, *right;
+    Node(int data)
+    {
+        this->data = data;
+        left = right = NULL;
+    }
+};
+ 
+/* Given a binary tree, print its nodes according to the
+"bottom-up" postorder traversal. */
+void printPostorder(struct Node* node)
+{
+    if (node == NULL)
+        return;
+ 
+    // first recur on left subtree
+    printPostorder(node->left);
+ 
+    // then recur on right subtree
+    printPostorder(node->right);
+ 
+    // now deal with the node
+    cout << node->data << " ";
+}
+ 
+/* Given a binary tree, print its nodes in inorder*/
+void printInorder(struct Node* node)
+{
+    if (node == NULL)
+        return;
+ 
+    /* first recur on left child */
+    printInorder(node->left);
+ 
+    /* then print the data of node */
+    cout << node->data << " ";
+ 
+    /* now recur on right child */
+    printInorder(node->right);
+}
+ 
+/* Given a binary tree, print its nodes in preorder*/
+void printPreorder(struct Node* node)
+{
+    if (node == NULL)
+        return;
+ 
+    /* first print data of node */
+    cout << node->data << " ";
+ 
+    /* then recur on left sutree */
+    printPreorder(node->left);
+ 
+    /* now recur on right subtree */
+    printPreorder(node->right);
+}
+ 
+/* Driver program to test above functions*/
+int main()
+{
+    struct Node* root = new Node(14);
+    root->left = new Node(2);
+    root->right = new Node(11);
+    root->left->left = new Node(1);
+    root->left->right = new Node(3);
+    root->left->left->left = new Node(7);
+    root->left->right->left = new Node(40);
+    root->right->left = new Node(10);
+    root->right->right = new Node(30);
+ 
+    cout << "\nPreorder traversal of binary tree is \n";
+    printPreorder(root);
+ 
+    cout << "\nInorder traversal of binary tree is \n";
+    printInorder(root);
+ 
+    cout << "\nPostorder traversal of binary tree is \n";
+    printPostorder(root);
+ 
+    return 0;
+}
 //space for testing
